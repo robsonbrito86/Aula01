c3d697c83891eed2395a7702812d6f508d8f28ec
robsonbrito86 <robson.brito1586@gmail.com>
2024-12-07 19:21:23 -0300

    minha commit

---
+++ aula2.py
@@ -0,0 +1,9 @@
+class Calculadora:
+    def __init__(self, somar, subtrair, multiplicar, dividir):
+        self.somar=somar
+        self.subtrair=subtrair
+        self.multiplicar=multiplicar
+        self.dividir=dividir
+
+
+        
