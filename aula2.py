2f93862f51b848c68a932b9f8be21a132e3db80a
robsonbrito86 <robson.brito1586@gmail.com>
2024-12-07 19:52:12 -0300

    completo

--- aula2.py
+++ aula2.py
@@ -1,26 +1,25 @@
 class Calculadora:
-    def __init__(self, somar, subtrair, multiplicar, dividir):
-        self.somar=somar
-        self.subtrair=subtrair
-        self.multiplicar=multiplicar
-        self.dividir=dividir
+    def __init__(self):
+        
     
     def somar(self, a, b):
         return a + b
-        pass
-    
+        
     
     def subtrair(self, a, b):
         return a - b
-        pass
+
     
     def multiplicar(self, a, b):
         return a * b
-        pass
+        
     
     def dividir(self, a, b):
         return a == b
-        pass
+    
+    
+        
+
     
 
     
